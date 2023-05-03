60464fbbe8134077e3a06f33e20f7f9a2005dcb0
1auta <r.1auta97@gmail.com>
2023-05-03 21:35:36 +0300

    ff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 const burger = document.getElementById("burger");
-console.log(hello);
-console.log(burger);
+const navbar = document.getElementById("navbar");
+const body = document.querySelector(".body");
+
+burger.addEventListener("click", (e) => {
+  burger.classList.toggle("active");
+  navbar.classList.toggle("active");
+  body.classList.toggle("lock");
+});
